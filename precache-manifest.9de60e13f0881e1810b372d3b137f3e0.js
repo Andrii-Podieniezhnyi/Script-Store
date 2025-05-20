4116a22ac7decd7c58a2d573a108a00159067068
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-05-20 21:03:31 +0300

    Updates

---
+++ precache-manifest.9de60e13f0881e1810b372d3b137f3e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bff5a1387574ea4d5210226c70e6cb",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/index.html"
+  },
+  {
+    "revision": "4f2d1401fb3fad12308c",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "f54d2265f3ba36b03f44",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/css/main.1987e94a.chunk.css"
+  },
+  {
+    "revision": "4f2d1401fb3fad12308c",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/js/2.5bf2f149.chunk.js"
+  },
+  {
+    "revision": "f54d2265f3ba36b03f44",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/js/main.163e6d02.chunk.js"
+  },
+  {
+    "revision": "c65fc3305e9ed8155aff",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/js/runtime~main.01c04699.js"
+  },
+  {
+    "revision": "ec0607e311143f2370d8eb9c2507ac7c",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/media/pichu_ico.ec0607e3.webp"
+  },
+  {
+    "revision": "d3ada596cf5bcd856fb25bb172c39c1d",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/media/pikachu_ico.d3ada596.png"
+  },
+  {
+    "revision": "3e0a576373329b4b072f98d34caa220e",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/media/pikachu_ninja_ico.3e0a5763.png"
+  },
+  {
+    "revision": "c2a5a3af1f80c432edec89a96b9184a4",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/media/pokeBall_all.c2a5a3af.png"
+  },
+  {
+    "revision": "1a03bcf3c2a4bcd6ccadf8bb866320dd",
+    "url": "/Andrii-Podieniezhnyi/Script-Store/static/media/raichu_ico.1a03bcf3.webp"
+  }
+]);
